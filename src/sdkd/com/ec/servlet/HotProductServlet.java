c4600d64b2a7b6c78ca22ad052ec0d7939e48984
zhaoshuai <1046936829@qq.com>
2016-07-07 11:09:08 +0800

    赵帅 更新了热卖商品和特价商品的servlet

---
+++ src/sdkd/com/ec/servlet/HotProductServlet.java
@@ -0,0 +1,22 @@
+package sdkd.com.ec.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zhaoshuai on 2016/7/7.
+ */
+@WebServlet(name = "HotProductServlet")
+public class HotProductServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
